8cdd6a9651084d018c6f53d7b4bba1281a4a7109
Carlos Cheung <carloscheung1997@gmail.com>
2025-05-24 17:01:00 +0800

    init

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,52 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+-- Neotree
+map("n", "<C-n>", ":Neotree toggle<CR>")
+
+-- Buffer
+map("n", "<Tab>", "<cmd>bnext<cr>")
+map("n", "<S-Tab>", "<cmd>bprev<cr>")
+map("n", ";c", function()
+    Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
+
+-- Split window
+map("n", "ss", ":split<Return><C-w>w")
+map("n", "sv", ":vsplit<Return><C-w>w")
+
+-- Resize window
+map("n", "<C-w><left>", "<C-w><")
+map("n", "<C-w><right>", "<C-w>>")
+map("n", "<C-w><up>", "<C-w>+")
+map("n", "<C-w><down>", "<C-w>-")
+
+-- Telescope
+map("n", ";f", LazyVim.pick("files"), { desc = "Find File" })
+map("n", ";r", LazyVim.pick("live_grep"), { desc = "Grep" })
+
+-- Terminal
+map("n", "<C-4>", function()
+    Snacks.terminal(nil, { cwd = LazyVim.root() })
+end, { desc = "Toggle Terminal" })
+map("t", "<C-4>", "<cmd>close<cr>", { desc = "Toggle Terminal" })
+-- map({ "n", "t" }, "<C-4>", function()
+--     Snacks.terminal(nil, { cwd = LazyVim.root() })
+-- end, { desc = "Toggle Terminal" })
+
+-- Diagnostics
+map("n", "<C-,>", vim.diagnostic.goto_next)
+map("n", "<C-.>", vim.diagnostic.goto_prev)
+
+-- Plugins
+map("n", ";s", ":Spectre<CR>", { desc = "Spectre" })
+map("n", ";d", ":lua require('dbee').open()<CR>", { desc = "Dbee" })
+map("n", ";g", ":LazyGit<CR>", { desc = "LazyGit" })
+map("n", ";-", "<cmd>Oil<cr>", { desc = "Oil" })
+
+-- Delete Default Keymaps
+vim.keymap.del("n", ";")
+vim.keymap.del("n", "s")
